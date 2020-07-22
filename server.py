e55a1df83ec1df57b1c693ba5d5c71e08ee4e391
sho <s.osaka@primecast.co.jp>
2020-07-22 16:12:55 +0900

    first commit

---
+++ server.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
